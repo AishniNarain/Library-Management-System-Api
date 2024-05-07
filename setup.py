2e7c8289a84064d2355ad02f227af21ff72c783e
Aishni Narain <aishninarain@gmail.com>
2024-05-07 16:10:15 +0530

    Edited files

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='Library_Management_System_Api',
     version='0.1',
-    description='Continuous-Integration using GitHub Actions',
+    description='An api for library',
     author='Aishni',
     author_email='your@email.com',
     packages=find_packages(),
