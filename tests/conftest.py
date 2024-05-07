9a9a1ef35c644b2906f6d2e0ad709e5d93fa51e7
Aishni Narain <aishninarain@gmail.com>
2024-05-07 16:16:01 +0530

    Edited files

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'your_secret_key'
 def app():
     app = flask_app
     app.config['TESTING'] = True
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://username:password@localhost/db_name'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://username:password@localhost/database'
     
     with app.app_context():
         db.create_all()
