cf30ad015eb9d7adb32418e199736e52e982dd9c
Aishni Narain <aishninarain@gmail.com>
2024-05-07 15:08:36 +0530

    Edited files

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,8 +4,7 @@ sys.path.append("/home/aishni.narain@intelegencia.com/Desktop/Library System Api
 import datetime
 import pytest
 from unittest.mock import MagicMock,patch
-from app import app as flask_app
-from extensions import db
+from app import app as flask_app,db
 from flask_jwt_extended import jwt_required,create_access_token
 
 SECRET_KEY = 'your_secret_key'
