8cab20dd1a257da3ad2ddf9b68114811f3577bea
Aishni Narain <aishninarain@gmail.com>
2024-05-07 14:53:29 +0530

    Edited files

--- tests/unit/test_models.py
+++
@@ -1,6 +0,0 @@
-from models import User
-
-def test_new_user():
-    user = User(username ='testusername', email = 'test@email.com')
-    assert user.username == 'testusername'
-    assert user.email == 'test@email.com'
