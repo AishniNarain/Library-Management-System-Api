d588a27547bf4e52fef0cd6554f877d8f09f8a94
Aishni Narain <aishninarain@gmail.com>
2024-05-07 12:52:10 +0530

    Edited files

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='Continuous-Integration',
+    version='0.1',
+    description='Continuous-Integration using GitHub Actions',
+    author='Aishni',
+    author_email='your@email.com',
+    packages=find_packages(),
+    install_requires=[
+        'pytest',
+    ],
+)
