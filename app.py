15064f692a3cc2460383e21fd74e89195b3efd44
Aishni Narain <aishninarain@gmail.com>
2024-05-28 18:04:55 +0530

    Docker file

--- app.py
+++ app.py
@@ -23,7 +23,8 @@ ma.init_app(app)
 migrate = Migrate(app, db)
 mail.init_app(app)
 
-client = MongoClient("mongodb://localhost:27017/")
+# client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb://mongo-db:27017/")
 
 api = Api(app,version='1.0', title='Library Management System Api',description='This is a sample API documentation for Library Management System. This documentation will provide all details related to the operations performed in a library.')
 ns = api.namespace('api/v1', description="Version 1.0 of API")
