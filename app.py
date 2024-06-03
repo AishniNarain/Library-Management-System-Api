1b3eed47a06ee687090e473714ed9a83ec58929b
Aishni Narain <aishninarain@gmail.com>
2024-06-03 17:24:26 +0530

    Updated files

--- app.py
+++ app.py
@@ -23,9 +23,9 @@ ma.init_app(app)
 migrate = Migrate(app, db)
 mail.init_app(app)
 
-client = MongoClient("mongodb://localhost:27017/")
+# client = MongoClient("mongodb://localhost:27017/")
 # mongodb://172.29.0.2:27017/ for mongo-compass
-# client = MongoClient("mongodb://multi-db:27017/")
+client = MongoClient("mongodb://multi-db:27017/")
 
 api = Api(app,version='1.0', title='Library Management System Api',description='This is a sample API documentation for Library Management System. This documentation will provide all details related to the operations performed in a library.')
 ns = api.namespace('api/v1', description="Version 1.0 of API")
