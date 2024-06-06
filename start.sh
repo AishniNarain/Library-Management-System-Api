b0c5f9c6bf50d746969eda6be050fce339d1e0c8
Aishni Narain <aishninarain@gmail.com>
2024-06-06 12:32:29 +0530

    Updated files

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-source /home/aishni.narain@intelegencia.com/Desktop/Library\ System\ Api/Library-Management-System-Api/venv/bin/activate
+source /home/aishni.narain@intelegencia.com/Desktop/library_api/venv/bin/activate
 flask run --reload
